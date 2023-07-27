02339a4e0457115cfa0c7df0a2b682513f2b4e35
TuanPA2002 <phianhtuan31@gmail.com>
2023-07-27 21:40:32 +0700

    #9 UI phần NewsLetter, Footer

--- front-end/src/components/FooterComp/FooterComponent.jsx
+++ front-end/src/components/FooterComp/FooterComponent.jsx
@@ -1,9 +1,70 @@
 import React from "react";
+import { FaLocationArrow } from "react-icons/fa";
+import { BsTelephoneForward, BsEnvelopeCheck } from "react-icons/bs";
+import payment from "../../assets/images/product/payments.png";
 
 import "./FooterComponent.scss";
 
 const FooterComponent = () => {
-  return <div className="wrapper-containerFooterComp">Footer Component</div>;
+  return (
+    <div className="wrapper-footerComp">
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">About</div>
+          <div className="text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+            beatae nesciunt maiores molestiae dolore. Fuga, minus saepe facere
+            perspiciatis voluptate soluta aspernatur dolorum sed amet temporibus
+            in iusto eligendi expedita.
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Contact</div>
+          <div className="c-item">
+            <FaLocationArrow />
+            <div className="text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+              quas magnam animi impedit ducimus magni fuga molestias unde
+              repellat!
+            </div>
+          </div>
+          <div className="c-item">
+            <BsTelephoneForward />
+            <div className="text">Phone: 0336 526 9311</div>
+          </div>
+          <div className="c-item">
+            <BsEnvelopeCheck />
+            <div className="text">Email: tuanpa2k2@gmail.com</div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Categories</div>
+          <span className="text">Books English</span>
+          <span className="text">Máy tính</span>
+          <span className="text">Đồ dùng học tập</span>
+          <span className="text">Điện thoại</span>
+          <span className="text">Giày dép</span>
+          <span className="text">Quần áo</span>
+        </div>
+        <div className="col">
+          <div className="title">Page</div>
+          <span className="text">home</span>
+          <span className="text">about</span>
+          <span className="text">Contact us</span>
+          <span className="text">facebook</span>
+          <span className="text">React JS</span>
+        </div>
+      </div>
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <div className="text">
+            created by TuanPA - Latest Design Template.
+          </div>
+          <img src={payment} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FooterComponent;
--- front-end/src/components/NewLetterComp/NewLetterComponent.jsx
+++ front-end/src/components/NewLetterComp/NewLetterComponent.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { BsFacebook, BsInstagram, BsTwitter, BsTiktok } from "react-icons/bs";
 
 import "./NewLetterComponent.scss";
 
 const NewLetterComponent = () => {
   return (
-    <div className="wrapper-containerNewLetterComp">New Letter Component</div>
+    <div className="wrapper-containerNewLetterComp">
+      <div className="newsletter-content">
+        <span className="big-text">
+          hãy theo dõi chúng tôi để cập nhập nhiều sảng phẩm mới nhất
+        </span>
+        <div className="form">
+          <input type="text" placeholder="Nhập địa chỉ email..." />
+          <button>send mail</button>
+        </div>
+        <div className="text">Will be any more</div>
+        <div className="social-icons">
+          <BsFacebook />
+          <BsInstagram />
+          <BsTwitter />
+          <BsTiktok />
+        </div>
+      </div>
+    </div>
   );
 };
 
