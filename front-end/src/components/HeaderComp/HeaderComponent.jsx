22df34ca49a4d6dbd0c2e661e59c9f59d32b9ec1
TuanPA2002 <phianhtuan31@gmail.com>
2023-07-28 23:41:25 +0700

    #13 UI sử dụng library @react-tippy

--- front-end/src/components/HeaderComp/HeaderComponent.jsx
+++ front-end/src/components/HeaderComp/HeaderComponent.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Tippy from "@tippyjs/react/headless";
 import { useNavigate } from "react-router-dom";
+
 import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 import { CiMenuKebab } from "react-icons/ci";
 import { VscAccount } from "react-icons/vsc";
@@ -22,17 +24,40 @@ const HeaderComponent = () => {
             </button>
           </div>
           <div className="right">
-            <span className="text-account" onClick={() => navigate("/sign-in")}>
-              <VscAccount />
-              account
-            </span>
-            <div
-              className="cart-icon"
-              onClick={() => navigate("/productDetails")}
+            <Tippy
+              interactive
+              render={(attrs) => (
+                <div className="tippy-popper" tabIndex="-1" {...attrs}>
+                  <span>Đăng nhập</span>
+                </div>
+              )}
             >
-              <AiOutlineShoppingCart />
-              <span>5</span>
-            </div>
+              <span
+                className="text-account"
+                onClick={() => navigate("/sign-in")}
+              >
+                <VscAccount />
+                tài khoản
+              </span>
+            </Tippy>
+
+            <Tippy
+              interactive
+              render={(attrs) => (
+                <div className="tippy-popper" tabIndex="-1" {...attrs}>
+                  <span>Giỏ hàng</span>
+                </div>
+              )}
+            >
+              <div
+                className="cart-icon"
+                onClick={() => navigate("/productDetails")}
+              >
+                <AiOutlineShoppingCart />
+                <span>5</span>
+              </div>
+            </Tippy>
+
             <span className="btn-more">
               <CiMenuKebab />
             </span>
