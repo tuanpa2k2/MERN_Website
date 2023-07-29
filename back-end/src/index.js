faf167cf8614fb35c66c44a00b77d7dfbb73599d
TuanPA2002 <phianhtuan31@gmail.com>
2023-07-29 13:14:54 +0700

    #16 Thiết kế database và connect MongooDB

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-  return res.send("hello world...");
+  res.send("hello world...");
 });
+
+mongoose
+  .connect(
+    `mongodb+srv://tuanpa2k2:${process.env.MONGOO_DB}@mern-website.hnxnyqc.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connect mongoo db success...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log("server is running port: " + port);
