75bad944f2031233cecb1e95621e26fb0500a376
TuanPA2002 <phianhtuan31@gmail.com>
2023-07-29 22:11:53 +0700

    #18 Tạo API đăng ký người dùng ở backend

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -2,10 +2,30 @@ const UserService = require("../services/UserService");
 
 const createUser = async (req, res) => {
   try {
-    console.log(req.body);
-    const res = await UserService.createUser();
+    const { name, email, password, confirmPassword, phone } = req.body;
+    const regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isCheckEmail = regex.test(email);
 
-    return res.status(200).json(res);
+    if (!name || !email || !password || !confirmPassword || !phone) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is reqiured",
+      });
+    } else if (!isCheckEmail) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The input is email",
+      });
+    } else if (password !== confirmPassword) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The password is equal confirmPassword",
+      });
+    }
+
+    const response = await UserService.createUser(req.body);
+
+    return res.status(200).json(response);
   } catch (e) {
     return res.status(404).json({
       message: e,
--- back-end/src/services/UserService.js
+++ back-end/src/services/UserService.js
@@ -1,7 +1,36 @@
-const createUser = () => {
-  return new Promise((resolve, reject) => {
+const User = require("../models/UserModel");
+
+const createUser = (newUser) => {
+  return new Promise(async (resolve, reject) => {
+    const { name, email, password, confirmPassword, phone } = newUser;
+
     try {
-      resolve({});
+      const checkUser = await User.findOne({
+        email: email, //tìm email đã tông tại trong db chưa?
+      });
+
+      if (checkUser !== null) {
+        resolve({
+          status: "OK",
+          message: "Email đã tồn tại",
+        });
+      }
+
+      const createdUser = await User.create({
+        name,
+        email,
+        password,
+        confirmPassword,
+        phone,
+      });
+
+      if (createdUser) {
+        resolve({
+          status: "OK",
+          message: "Success",
+          data: createdUser,
+        });
+      }
     } catch (e) {
       reject(e);
     }
