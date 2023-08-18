d2990d7c874bc5e7ff920a85f60dc558d537a7f8
TuanPA2002 <phianhtuan31@gmail.com>
2023-08-18 11:06:42 +0700

    #54 Tích hợp redux persist với redux-toolkit

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import GlobalStyle from "./components/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
@@ -14,9 +15,11 @@ root.render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <GlobalStyle>
-        <App />
-      </GlobalStyle>
+      <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyle>
+          <App />
+        </GlobalStyle>
+      </PersistGate>
     </Provider>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
